ba069c2d99f276824a02907a73bc3caefb96fb7d
MaPhi Werner <maphi@codecentric.de>
2018-05-21 10:05:57 +0200

    Added cglib-nodep for class mocking

--- src/test/groovy/D_MocksAndStubs.groovy
+++ src/test/groovy/D_MocksAndStubs.groovy
@@ -1,8 +1,15 @@
 import spock.lang.Specification
 
+@SuppressWarnings("GroovyAssignabilityCheck")
 class D_MocksAndStubs extends Specification {
     interface Data {
         int retrieve(String key)
+    }
+
+    class DataClass implements Data {
+        int retrieve (String key) {
+            return 0
+        }
     }
 
     private static int checkData(Data myData, String key) { return myData.retrieve(key) }
@@ -16,11 +23,12 @@ class D_MocksAndStubs extends Specification {
 
         then:
         1 * myData.retrieve(_)
+        0 * _
     }
 
     def "checkData is working as expected"() {
         given:
-        def myData = Mock(Data)
+        Data myData = Mock()
         myData.retrieve("keyString") >> { 3 }
         myData.retrieve("otherString") >> { 5 }
 
@@ -38,5 +46,16 @@ class D_MocksAndStubs extends Specification {
         expect:
         3 == checkData(myData, "keyString")
         5 == checkData(myData, "otherString")
+    }
+
+    def "mocking the DataClass"() {
+        given:
+        DataClass dataClass = Mock()
+        1 * dataClass.retrieve("keyString") >> { 3 }
+        1 * dataClass.retrieve("otherString") >> { 5 }
+
+        expect:
+        3 == checkData(dataClass, "keyString")
+        5 == checkData(dataClass, "otherString")
     }
 }
