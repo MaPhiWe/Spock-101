bf21414bf3d7e75d384449024f4990179238be19
MaPhi Werner <maphi@codecentric.de>
2018-05-30 08:49:05 +0200

    given/when/then and setup/cleanup examples

--- src/test/groovy/A_SimpleStart.groovy
+++ src/test/groovy/A_SimpleStart.groovy
@@ -8,4 +8,39 @@ class A_SimpleStart extends Specification {
         expect:
         1+2 == 3
     }
+
+    void "The += operator concatenates two Strings"() {
+
+        given: "the first half of the best-known String in IT"
+        String myString = "Hello"
+
+        when: "the second half is concatenated to it"
+        myString += " World"
+
+        then: "the result is the sum of both parts"
+        myString == "Hello World"
+        myString.length() == 11
+    }
+
+    class Database {
+        int dbsize = 0
+        void insert(def content) { ++dbsize}
+        int size() { dbsize }
+        void close() { dbsize = 0 }
+
+    }
+
+    void "Inserting increases the DB size by one"() {
+        setup: "Initialising the database"
+        Database db = new Database()
+
+        when:
+        db.insert("Hello World")
+
+        then:
+        db.size() == 1
+
+        cleanup:
+        db.close()
+    }
 }
